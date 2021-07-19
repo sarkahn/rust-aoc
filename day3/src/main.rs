096e3782eca2e6cf708409e480a9a4cb5a42fe4f
sark <sarkahn@gmail.com>
2021-07-19 09:42:52 -0600

    Day 3 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,12 +8,11 @@ struct Map {
 
 impl Map {
     fn new(string: &str) -> Map {
-        let map = fs::read_to_string("data/map.txt").expect("Error reading map");
-        let lines: Vec<&str> = map.lines().collect();
+        let lines: Vec<&str> = string.lines().collect();
         
         let width = lines[0].replace('\r', "").replace('\n', "").len();
         let height = lines.len();
-        let map = map.replace('\n', "").replace('\r', "");
+        let map = string.replace('\n', "").replace('\r', "");
         let chars = map.chars().collect();
 
         Map {
@@ -27,28 +26,13 @@ impl Map {
         self.chars[y * self.width  + x]
     }
 
-    fn advance(&self, x: &mut usize, y: &mut usize) {
-        *x = (*x + 3) % self.width;
-        *y = *y + 1;
+    fn advance(&self, x: &mut usize, y: &mut usize, slope: (usize, usize)) {
+        *x = (*x + slope.0) % self.width;
+        *y = *y + slope.1;
     }
 }
 
-
-fn read_position(x:usize, y:usize, width: usize, map: &Vec<char>) -> char {
-    map[y * width  + x]
-}
-
-fn advance(x: &mut usize, y: &mut usize, width: usize) {
-    *x = (*x + 3) % width;
-    *y = *y + 1;
-}
-
-fn main() {
-    let map = fs::read_to_string("data/map.txt").expect("Error reading map");
-    let map = Map::new(&map);
-
-    println!("Map size {}, {}", map.width, map.height);
-    
+fn count_trees_along_slope(slope: (usize,usize), map: &Map) -> usize {
     let mut x:usize = 0;
     let mut y:usize = 0;
 
@@ -60,8 +44,42 @@ fn main() {
             tree_amount = tree_amount + 1;
         }
 
-        map.advance(&mut x, &mut y);
+        map.advance(&mut x, &mut y, slope);
     }
 
+    tree_amount
+}
+
+fn main() {
+    let map_string = fs::read_to_string("data/map.txt").expect("Error reading map");
+    let map = Map::new(&map_string);
+
+    println!("Map size {}, {}", map.width, map.height);
+
+    let tree_amount = count_trees_along_slope((3,1), &map);
+
     println!("{} trees encountered", tree_amount);
+
+    // Part 2
+    let mut trees_product = 0;
+
+    let slopes = [
+        (1,1),
+        (3,1),
+        (5,1),
+        (7,1),
+        (1,2),
+    ];
+
+    for slope in slopes {
+        let tree_amount = count_trees_along_slope(slope, &map);
+
+        if trees_product == 0 {
+            trees_product = tree_amount;
+        } else {
+            trees_product = trees_product * tree_amount;
+        }
+    }
+
+    println!("Combined product of slopes: {}", trees_product);
 }
